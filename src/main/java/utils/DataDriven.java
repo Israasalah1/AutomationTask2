52c306fb7433ad30122ac713aa88bfe616a16748
Israasalah1 <israasalah208@gmail.com>
2026-02-24 14:38:03 +0200

    Part 2 - Added CartPage, CartTest, and social/cart validations

--- src/main/java/utils/DataDriven.java
+++ src/main/java/utils/DataDriven.java
@@ -1,4 +1,33 @@
 package utils;
 
+import org.json.JSONObject;
+
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Scanner;
+import org.json.JSONObject;
+import java.io.InputStream;
+import java.util.Scanner;
+
+
 public class DataDriven {
+
+    public static JSONObject jsonReader() {
+        try {
+            InputStream is = DataDriven.class
+                    .getClassLoader()
+                    .getResourceAsStream("testData.json");
+
+            Scanner scanner = new Scanner(is).useDelimiter("\\A");
+            String content = scanner.hasNext() ? scanner.next() : "";
+
+            return new JSONObject(content);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
 }
--- src/test/java/Tests/InventoryTest.java
+++ src/test/java/Tests/InventoryTest.java
@@ -1,4 +1,29 @@
 package Tests;
 
-public class InventoryTest {
+import base.BaseTest;
+import org.json.JSONObject;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import Pages.InventoryPage;
+import Pages.LoginPage;
+import utils.DataDriven;
+
+public class InventoryTest extends BaseTest {
+
+    JSONObject data = DataDriven.jsonReader();
+
+    @Test
+    public void verifyInventoryPage() {
+
+        LoginPage login = new LoginPage(driver);
+        login.enterUsername(data.getString("validUser"));
+        login.enterPassword(data.getString("validPass"));
+        login.clickLogin();
+
+        InventoryPage inventory = new InventoryPage(driver);
+
+        Assert.assertEquals(inventory.getPageTitle(), "Swag Labs");
+        Assert.assertTrue(inventory.isCartDisplayed());
+        Assert.assertEquals(inventory.getProductsCount(), 6);
+    }
 }
